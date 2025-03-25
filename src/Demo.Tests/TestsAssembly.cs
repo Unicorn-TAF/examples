d6a1b424e409e6c462e427789f5f044331ab47aa
Vitaliy Dobriyan <dobriy88@yandex.ru>
2025-03-25 21:51:55 +0300

    updated ui.core lib version, added reporting switcher

--- src/Demo.Tests/TestsAssembly.cs
+++ src/Demo.Tests/TestsAssembly.cs
@@ -1,11 +1,14 @@
 ﻿using Demo.Commons;
 using Demo.Tests.Handlers;
+using System;
 using Unicorn.Reporting.Allure;
 using Unicorn.Reporting.ReportPortal;
+using Unicorn.Reporting.TestIt;
 using Unicorn.Taf.Api;
 using Unicorn.Taf.Core;
 using Unicorn.Taf.Core.Logging;
 using Unicorn.Taf.Core.Testing.Attributes;
+using Unicorn.UI.Core;
 using Unicorn.UI.Win;
 
 namespace Demo.Tests
@@ -18,7 +21,7 @@ namespace Demo.Tests
     {
         private static ITestReporter reporter;
         private static GitHubBts bts;
-        private static WinScreenshotTaker screenshotter;
+        private static ScreenshotTakerBase screenshotter;
 
         /// <summary>
         /// Actions before all tests execution.
@@ -39,23 +42,33 @@ namespace Demo.Tests
             Config.DependentTests = TestsDependency.Skip;
 
             // Initialize built-in screenshotter with automatic subscription to test fail event.
-            // By default the screenshotter will be initialized for Screenshots directory in test binaries dir.
+            // By default the screenshotter will be initialized in ./Screenshots directory in test binaries dir.
             screenshotter = new WinScreenshotTaker();
             screenshotter.SubscribeToTafEvents();
 
             bts = new GitHubBts();
 
-            // Initialize built-in Allure reporter with automatic subscription to all testing events.
-            // allureConfig.json should exist in binaries directory.
-            reporter = new AllureReporter();
+            // It's possible to report to several supported reporting systems
+            Type reporterType = typeof(AllureReporter);
 
-            // Initialize built-in Report Portal reporter with automatic subscription to all testing events.
-            // ReportPortal.config.json should exist in binaries directory.
-            //reporter = new ReportPortalReporter();
-
-            // Initialize built-in TestIT reporter with automatic subscription to all testing events.
-            // Tms.config.json should exist in binaries directory.
-            //reporter = new TestItReporter();
+            switch (reporterType.Name)
+            {
+                case "AllureReporter":
+                    // Initialize built-in Allure reporter with automatic subscription to all testing events.
+                    // allureConfig.json should exist in binaries directory.
+                    reporter = new AllureReporter();
+                    break;
+                case "ReportPortalReporter":
+                    // Initialize built-in Report Portal reporter with automatic subscription to all testing events.
+                    // ReportPortal.config.json should exist in binaries directory.
+                    reporter = new ReportPortalReporter();
+                    break;
+                case "TestItReporter":
+                    // Initialize built-in TestIT reporter with automatic subscription to all testing events.
+                    // Tms.config.json should exist in binaries directory.
+                    reporter = new TestItReporter();
+                    break;
+            }
         }
 
         /// <summary>
@@ -69,7 +82,7 @@ namespace Demo.Tests
             reporter?.Dispose();
             reporter = null;
 
-            // Unsubscribing screenshotter from unicorn events.
+            // Unsubscribe screenshotter from unicorn events.
             screenshotter.UnsubscribeFromTafEvents();
             screenshotter = null;
 
